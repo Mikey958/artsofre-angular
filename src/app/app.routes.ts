4e5e0ad6649652921425ea536c1667f4b4a40391
Andrei Stanovykh <astanovykh@gmail.com>
2025-10-22 04:15:46 +0500

    feat: implement layout and routing structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { CompanyDetail } from './features/companies/pages/company-detail/company-detail';
+import { CompanyList } from './features/companies/pages/company-list/company-list';
+import { CompanyYandexMap } from './features/companies/pages/company-yandex-map/company-yandex-map';
+import { Layout } from './layouts/layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full',
+      },
+      {
+        path: 'list',
+        component: CompanyList,
+      },
+      {
+        path: 'detail/:id',
+        component: CompanyDetail,
+      },
+      {
+        path: 'map',
+        component: CompanyYandexMap,
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'list',
+  },
+];
