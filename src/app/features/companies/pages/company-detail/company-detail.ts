b60f058de48a22e2655113071e35483cd79c3f53
Andrei Stanovykh <astanovykh@gmail.com>
2025-10-23 19:35:40 +0500

    feat: добавлена страница детальной информации о компании

--- src/app/features/companies/pages/company-detail/company-detail.ts
+++ src/app/features/companies/pages/company-detail/company-detail.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Company } from '../../models/company';
+import { CompanyService } from './../../../../core/services/company.service';
 
 @Component({
   selector: 'app-company-detail',
   imports: [],
   templateUrl: './company-detail.html',
-  styleUrl: './company-detail.scss'
+  styleUrl: './company-detail.scss',
 })
-export class CompanyDetail {
+export class CompanyDetail implements OnInit {
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private companyService = inject(CompanyService);
 
+  company = signal<Company | null>(null);
+  isLoading = signal(true);
+  hasError = signal(false);
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.loadCompany(+id);
+    } else {
+      this.goBack();
+    }
+  }
+
+  loadCompany(id: number) {
+    this.isLoading.set(true);
+    this.hasError.set(false);
+
+    this.companyService.getCompanyById(id).subscribe({
+      next: (data) => {
+        this.company.set(data);
+        this.isLoading.set(false);
+      },
+      error: (err) => {
+        console.error('Error loading company', err);
+        this.isLoading.set(false);
+        this.hasError.set(true);
+      },
+    });
+  }
+
+  goBack() {
+    this.router.navigate(['/list']);
+  }
 }
